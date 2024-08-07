850e5cf4304684a8cdffedf5e6f4990986ef933b
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-08-07 20:55:41 +0200

    computer class created with tests

--- src/main/java/dev/hugo/Computer.java
+++ src/main/java/dev/hugo/Computer.java
@@ -1,5 +1,67 @@
 package dev.hugo;
 
 public class Computer {
-    
+
+    private String brand;
+    private int memory; // Memory in GB
+    private String processor;
+    private String operatingSystem;
+    private double price;
+
+    // Constructor
+    public Computer(String brand, int memory, String processor, String operatingSystem, double price) {
+        this.brand = brand;
+        this.memory = memory;
+        this.processor = processor;
+        this.operatingSystem = operatingSystem;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer [brand=" + brand + ", memory=" + memory + "GB, processor=" + processor +
+                ", operatingSystem=" + operatingSystem + ", price=" + price + "]";
+    }
 }
+
